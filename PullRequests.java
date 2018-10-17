30539b7e861c4f2f79b423934ceb62e314a30c39
nervcv <nervcv@gmail.com>
2018-10-17 16:39:39 +0900

    PullRequests 클래스 주석 달기

--- PullRequests.java
+++ PullRequests.java
@@ -1,9 +1,11 @@
 /**
+ * PullRequest 테스트 클래스
+ * 
  * @author : nervcv
  * @version : 1.0
  */
 public class PullRequests {
-    
+
     public static void main(String args[]){
         print(msg);
     }
